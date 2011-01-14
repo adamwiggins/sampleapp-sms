d72e59a3ad5b68ac83e1bfc2f6d64c32c0900108
Adam Wiggins <adam@heroku.com>
2011-01-14 13:42:27 -0800

    init

---
+++ web.js
@@ -0,0 +1,12 @@
+require.paths.unshift('./node_modules');
+
+var express = require('express');
+var app = express.createServer();
+
+app.get('/', function(req, res){
+    res.send('Hello World');
+});
+
+var port = parseInt(process.env.PORT || 3000);
+console.log("Listening on port " + port);
+app.listen(port);
