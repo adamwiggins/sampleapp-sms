97379a3e363b0c40ee3c34470c7db077d2d34f41
Adam Wiggins <adam@heroku.com>
2011-01-14 13:54:22 -0800

    web form

--- web.js
+++ web.js
@@ -2,11 +2,16 @@ require.paths.unshift('./node_modules');
 
 var express = require('express');
 var app = express.createServer(
-  express.logger()
+  express.logger(),
+  express.bodyDecoder()
 );
 
-app.get('/', function(req, res){
-    res.send('Hello World');
+app.get('/', function(req, res) {
+  res.send('<form action="/" method="post">Msg: <input type="text" name="msg" /><input type="submit" /></form>');
+});
+
+app.post('/', function(req, res) {
+  res.send('You said: ' + req.body.msg);
 });
 
 var port = parseInt(process.env.PORT || 3000);
