0b8d1ad1ebb388a62e3a8208b6960f79ebe9f7ba
Adam Wiggins <adam@heroku.com>
2011-01-14 14:03:56 -0800

    moonshado sms gateway

--- worker.js
+++ worker.js
@@ -1,10 +1,21 @@
 require.paths.unshift('./node_modules');
 
 var queue = require('./queue');
+var restclient = require('./restclient/restclient');
 
 send_sms = function(args, finish) {
-  console.log("hello from job");
-  finish();
+  url = process.env.MOONSHADOSMS_URL;
+  if (url) {
+    restclient.POST(process.env.MOONSHADOSMS_URL + "/sms",
+      { 'sms[device_address]': args.mobile_number, 'sms[message]': args.msg },
+      function(res) {
+        console.log("Got response: " + res);
+        finish();
+      });
+  } else {
+    console.log("no moonshado resource");
+    finish();
+  }
 }
 
 queue.run_worker({ send_sms: send_sms });
